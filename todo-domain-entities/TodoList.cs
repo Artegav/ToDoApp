c18f1c1755c90a54db2d7ac1f0e4d1a8c284a867
artemiygavrilov <gavrilow97@ya.ru>
2023-05-15 17:54:33 +0600

    create base project with CRUD

---
+++ todo-domain-entities/TodoList.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace todo_domain_entities;
+
+public class TodoList
+{
+    public int Id { get; set; }
+    
+    [Required]
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+    
+    public bool? IsHidden { get; set; }
+
+    public List<TodoItem>? Items { get; set; }
+}
